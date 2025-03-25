d8436ab0f7f57ce91a981c5f0d6ad17b852974ec
chrisworthcgi <christopher.worthington@hmcts.net>
2025-03-25 11:14:44 +0000

    park work

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {FeesComponent} from './fees/fees.component';
+import {FeeComponent} from './fee/fee.component';
+import {UploadComponent} from './upload/upload.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'fees', component: FeesComponent},
+  { path: 'fee', component: FeeComponent},
+  { path: 'upload', component: UploadComponent}
+];
+
