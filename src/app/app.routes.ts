883377dfb468d533fd2822d3f88f7063d00941dc
chrisworthcgi <christopher.worthington@hmcts.net>
2025-04-03 06:56:48 +0100

    park work

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import {FeesComponent} from './fees/fees.component';
+import {FeeListComponent} from './fee-list/fee-list.component';
 import {FeeComponent} from './fee/fee.component';
 import {UploadComponent} from './upload/upload.component';
 
 export const routes: Routes = [
-  { path: 'fees', component: FeesComponent},
+  { path: 'fees', component: FeeListComponent},
   { path: 'fee', component: FeeComponent},
   { path: 'upload', component: UploadComponent}
 ];
