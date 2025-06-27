ace84179b113f9b28beb4aa6e034a7e1b1b79f28
chrisworthcgi <christopher.worthington@hmcts.net>
2025-06-27 10:58:59 +0100

    park

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import {FeeListComponent} from './fee-list/fee-list.component';
 import {FeeDetailsComponent} from './fee-details/fee-details.component';
 import {FeeCreateComponent} from './fee-create/fee-create.component';
+import {DatatableComponent} from './datatable/datatable.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'fees', pathMatch: 'full' },
   { path: 'fees', component: FeeListComponent},
   { path: 'feedetails/:feeCode', component: FeeDetailsComponent},
-  { path: 'create', component: FeeCreateComponent}
+  { path: 'create', component: FeeCreateComponent},
+  { path: 'datatable', component: DatatableComponent}
 ];
 
